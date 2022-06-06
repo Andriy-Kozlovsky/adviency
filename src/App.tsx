210a31442d8ab0194c5fb4bd5696f8637019e5ae
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-05 22:06:44 -0300

    day 4 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./styles.css";
 
 export default function App() {
+  const [list, setList] = useState<string[]>([]);
+
+  function onAddGift(event: any) {
+    event.preventDefault();
+    setList([...list, event.target["gift"].value]);
+    event.target["gift"].value = "";
+  }
+
+  function onRemoveGift(i: number) {
+    setList((prevList) => {
+      return prevList.filter((item, index) => index !== i);
+    });
+  }
+
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="container">
+        <h1>Regalos:</h1>
+        <form onSubmit={onAddGift}>
+          <input name="gift" type="text" />
+          <button type="submit">Agregar</button>
+        </form>
+        <ul>
+          {list.map((item, i) => (
+            <li key={i}>
+              {item}
+              <button onClick={() => onRemoveGift(i)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
