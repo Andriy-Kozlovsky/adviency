d120995aca78d0a5fea363871f58d9020424045c
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-06 18:54:58 -0300

    day 5 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./styles.css";
 
 export default function App() {
+  const [list, setList] = useState<string[]>([]);
+
+  async function onSubmitGiftHandler(event: any) {
+    event.preventDefault();
+    await setList((prevList) => [...prevList, event.target["gift"].value]);
+    event.target["gift"].value = "";
+  }
+
+  function onRemoveGiftHandler(index: number) {
+    setList((prevList) => {
+      return prevList.filter((gift, i) => i !== index);
+    });
+  }
+
+  function onRemoveAllGiftsHandler() {
+    setList([]);
+  }
+
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="container">
+        <h1>Regalos:</h1>
+        <form onSubmit={onSubmitGiftHandler}>
+          <input name="gift" type="text" />
+          <button type="submit">Agregar</button>
+        </form>
+        <ul>
+          {list.map((gift, index) => (
+            <li key={index}>
+              {gift}
+              <button onClick={() => onRemoveGiftHandler(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+        <button className="borrar-todo-btn" onClick={() => onRemoveAllGiftsHandler()}>
+          Borrar todo
+        </button>
+      </div>
     </div>
   );
 }
