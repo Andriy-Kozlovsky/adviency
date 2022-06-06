9dd1339c9a36bbe768e232341807556efe478eab
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-05 21:25:42 -0300

    day 3 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./styles.css";
 
 export default function App() {
+  const [list, setList] = useState<any>([]);
+
+  function onAddItem(event: any) {
+    event.preventDefault();
+    setList([...list, event.target["text"].value]);
+  }
+
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="gifts">
+        <h1>Regalos:</h1>
+        <form onSubmit={onAddItem}>
+          <input name="text" type="text" />
+          <button type="submit">Agregar</button>
+        </form>
+        <ul>
+          {list.map((item: any) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
