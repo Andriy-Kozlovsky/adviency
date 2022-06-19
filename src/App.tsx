53d0af13473e1a742bf7fefbeeeb9c69b65fc936
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-19 17:16:05 -0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,3 @@
-import Form from "./components/Form/Form";
-import List from "./components/List/List";
-import useApp from "./hooks/use-app";
-import "./styles.css";
-
 export default function App() {
-  const {
-    openedForm,
-    list,
-    removeItem,
-    clearList,
-    getObjHandler,
-    openForm,
-    resetItemToEdit,
-    itemToEdit,
-    editItem,
-    loading,
-  } = useApp();
-
-  return (
-    <div className="app">
-      <h1>Regalos:</h1>
-      <button className="btn-primary" onClick={() => openForm()}>
-        Agregar regalo
-      </button>
-      {loading ? (
-        <p>Cargando...</p>
-      ) : (
-        <List editItem={editItem} list={list} removeItem={removeItem} />
-      )}
-      <button onClick={() => clearList()}>Borrar todo</button>
-      {openedForm && (
-        <Form itemToEdit={itemToEdit} resetItemToEdit={resetItemToEdit} sendObj={getObjHandler} />
-      )}
-    </div>
-  );
+  return <div />;
 }
