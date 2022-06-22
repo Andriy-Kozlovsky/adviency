19482d9001ba8a143946f044e449e132244a5684
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-22 20:38:08 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import List from "./components/List";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Regalos:</h1>
-      <Button primary>Agregar regalo</Button>
-      <List />
-      <Button primary={false}>Borrar todo</Button>
-      <Form />
+    <div className="h-screen bg-[url('./images/fondo-navidad.jpg')] pt-14">
+      <div className="max-w-sm mx-auto bg-white rounded-md p-6 flex flex-col">
+        <h1 className="text-red-800 text-2xl">Regalos:</h1>
+        <Button primary>Agregar regalo</Button>
+        <List />
+        <Button primary={false}>Borrar todo</Button>
+        <Form />
+      </div>
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,16 +2,18 @@ import Button from "./Button";
 
 const List = () => {
   return (
-    <ul>
-      <li>
-        <img alt="" src="" />
+    <ul className="flex flex-col gap-4">
+      <li className="flex justify-between">
+        <img alt="" className="h-10 w-10 my-auto" src="" />
         <div>
-          <p>
+          <p className="flex flex-col">
             Medias (3) - $ 300,00 <span>Goncy</span>
           </p>
         </div>
-        <Button primary={false}>E</Button>
-        <Button primary={false}>X</Button>
+        <div className="flex gap-2 py-3">
+          <Button primary={false}>E</Button>
+          <Button primary={false}>X</Button>
+        </div>
       </li>
     </ul>
   );
